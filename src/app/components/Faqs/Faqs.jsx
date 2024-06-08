7cde0cbe049c9df3d305a8fb66b1702457735e2f
Shruti-2303 <sharmas23032001@gmail.com>
2024-06-09 00:26:13 +0530

    Feat : Added FAQ section

--- src/app/components/Faqs/Faqs.jsx
+++ src/app/components/Faqs/Faqs.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import styles from './faqs.module.scss'
+"use client";
+import React from "react";
+import styles from "./faqs.module.scss";
+import { Collapse } from "antd";
+import { faqItems } from "@/app/utils/constants";
 
 const Faqs = () => {
   return (
     <div className={styles.faqsContainer}>
       <div className={styles.faqs}>
-        
+        <div className={styles.heading}>
+          <span className={styles.headingBlue}>FAQs</span>
+        </div>
+        <div className={styles.faqsCollapse}>
+          <Collapse items={faqItems} defaultActiveKey={["1"]} accordion />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Faqs
+export default Faqs;
---
+++ src/app/utils/constants.js
@@ -0,0 +1,124 @@
+export const whyToAttendData = [
+  {
+    img: `${process.env.CLOUDINARY_URL}/v1717854753/UniConnect/2_knepwr_tj07x8.png`,
+    title: "Meet Top University Representatives",
+    description:
+      "Interact one on one with top university representatives and know about the campus and their course details, application process, and future career paths after the course",
+  },
+  {
+    img: `${process.env.CLOUDINARY_URL}/v1717854753/UniConnect/1_qq1k2e_wleulq.png`,
+    title: "Get On-Spot Offers",
+    description:
+      "Apply with one click inside the booth and secure on-spot offers from top universities",
+  },
+  {
+    img: `${process.env.CLOUDINARY_URL}/v1717854758/UniConnect/4_qfxu6h_snx4ai.png`,
+    title: "Application Guidance",
+    description:
+      "Get application assistance and guidance by top university representatives and Leverage Edu experts who will handhold you at every step",
+  },
+  {
+    img: `${process.env.CLOUDINARY_URL}/v1717854753/UniConnect/3_reyvlh_y7klmm.png`,
+    title: "Scholarship Guidance",
+    description:
+      "Find out how to qualify for generous scholarships and educational loans, in exclusive webinars and presentations",
+  },
+];
+
+export const faqItems = [
+  {
+    key: "1",
+    label: "What should I do after registering for UniConnect? ",
+    children: (
+      <p>
+        {" "}
+        Once you have registered, the next step is to complete your profile.
+        Based on your preferred study location and course, you will get a list
+        of universities that perfectly match your profile. Shortlist
+        universities that interest you, prepare queries regarding the
+        application, scholarship, eligibility, etc. and attend the upcoming
+        UniConnect to interact 1:1 with university representatives.{" "}
+      </p>
+    ),
+  },
+  {
+    key: "2",
+    label: "What do you need to do before attending the UniConnect event? ",
+    children: (
+      <p>
+        You will be interacting directly with the university representatives in
+        the UniConnect event. Prepare a list of questions around your course,
+        application, scholarship, fees, or any other study abroad query. The
+        university admission experts are there to help you throughout the
+        application process and enlighten you on the student life at their
+        university.{" "}
+      </p>
+    ),
+  },
+  {
+    key: "3",
+    label: "How to apply to a university after entering a booth? ",
+    children: (
+      <p>
+        Once all your doubts are addressed and you have finalised your desired
+        university, you can click on the ‘Apply Now’ button at the top of the
+        booth. Our counsellors will get in touch with you to discuss the
+        application details.
+      </p>
+    ),
+  },
+  {
+    key: "4",
+    label: "What is the fee to attend UniConnect? ",
+    children: <p>UniConnect is free of cost for all students.</p>,
+  },
+  {
+    key: "5",
+    label: "Can I join anytime during the event? ",
+    children: (
+      <p>
+        Yes you can join the UniConnect events anytime during the stipulated
+        event timings. However, slots at each university booth are limited,
+        hence, we recommend that you join the event as early as possible.
+      </p>
+    ),
+  },
+  {
+    key: "6",
+    label: "I cannot see the ‘Enter Now’ button on my screen.  ",
+    children: (
+      <p>
+        The ‘Enter Now’ button is visible once the event is live. Before that,
+        you can view university details and bookmark them according to your
+        preference.
+      </p>
+    ),
+  },
+  {
+    key: "7",
+    label: "What does Recommended tab signify on the event page? ",
+    children: (
+      <p>
+        The recommended tab has a list of universities that are best suited for
+        your profile.
+      </p>
+    ),
+  },
+  {
+    key: "8",
+    label: "My microphone/audio is not working after entering the booth. ",
+    children: (
+      <p>
+        Please make sure you click on the ‘Microphone’ symbol when asked to
+        select your preference and provide permission for the same. If you still
+        face any issues, please get in touch with us at{" "}
+        <a
+          style={{ color: "#0487F3" }}
+          href="mailto:supportuniconnect@leverageedu.com"
+        >
+          supportuniconnect@leverageedu.com
+        </a>
+      </p>
+    ),
+  },
+];
